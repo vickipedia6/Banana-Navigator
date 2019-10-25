46e10ed313242255686ba6353985462f90c906fa
Vignesh C <vignesh.c.consultant@nielsen.com>
2019-10-26 00:59:19 +0530

    First Commit

---
+++ model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed,fc1_units=96, fc2_units=96,fc3_units=96):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, fc3_units)
+        self.fc4 = nn.Linear(fc3_units , action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.tanh(self.fc1(state))
+        x = F.tanh(self.fc2(x))
+        x = F.tanh(self.fc3(x))
+        return self.fc4(x)
